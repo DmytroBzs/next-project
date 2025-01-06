e2939d45c134fe7d4e9c754bc2ef174302bc87ea
DmytroBzs <godimmortalis@gmail.com>
2025-01-06 14:43:48 +0200

    companies page almost done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+import { headers } from 'next/headers';
+import AddCompanyButton from './components/add-company-button';
+
 export default function Home() {
+  console.log(headers());
   return (
     <main>
-      <h1>Main Page</h1>
+      <h1 className="text-xl">Main Page {new Date().toTimeString()}</h1>
+      <AddCompanyButton />
     </main>
   );
 }
