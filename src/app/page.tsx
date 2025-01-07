74f6ed4efadb562982deac492453b5c62c95f5c9
DmytroBzs <godimmortalis@gmail.com>
2025-01-07 16:20:57 +0200

    finish

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,17 @@
 import { headers } from 'next/headers';
-import AddCompanyButton from './components/add-company-button';
+import Link from 'next/link';
 
 export default function Home() {
   console.log(headers());
   return (
     <main>
-      <h1 className="text-xl">Main Page {new Date().toTimeString()}</h1>
-      <AddCompanyButton />
+      <h1 className="text-xl mb-5">Main Page</h1>
+      <Link
+        className="py-2.5 px-5 bg-gray-900 text-zinc-50 text-base text-center font-medium rounded"
+        href={'/dashboard'}
+      >
+        Dashboard
+      </Link>
     </main>
   );
 }
